03a4c5ef99b784e7621152b99069eb50337c9c4c
Hartanto <tanyatantociputat@gmail.com>
2020-07-07 15:20:28 +0700

    perbaikan list golongan

--- resources/views/golongan/index.blade.php
+++ resources/views/golongan/index.blade.php
@@ -1,62 +1,71 @@
 
 @extends('layouts/contentLayoutMaster')
 
-@section('title', 'List View')
+@section('title', 'Data Golongan')
 
 @section('vendor-style')
-        {{-- vendor files --}}
+        {{-- vendor css files --}}
         <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/datatables.min.css')) }}">
-        <link rel="stylesheet" href="{{ asset(mix('vendors/css/file-uploaders/dropzone.min.css')) }}">
-        <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/extensions/dataTables.checkboxes.css')) }}">
-@endsection
-@section('page-style')
-        {{-- Page css files --}}
-        <link rel="stylesheet" href="{{ asset(mix('css/plugins/file-uploaders/dropzone.css')) }}">
-        <link rel="stylesheet" href="{{ asset(mix('css/pages/data-list-view.css')) }}">
 @endsection
 
 @section('content')
-{{-- Data list view starts --}}
-<section id="data-list-view" class="data-list-view-header">
-   
-    {{-- DataTable starts --}}
-    <div class="table-responsive">
-      <table class="table data-list-view">
-        <thead>
-          <tr>
-            <th></th>
-            <th>NAME</th>
-            <th>CATEGORY</th>
-            <th>POPULARITY</th>
-            <th>ORDER STATUS</th>
-            <th>PRICE</th>
-            <th>ACTION</th>
-          </tr>
-        </thead>
-        <tbody>
-          
-        </tbody>
-      </table>
-    </div>
-    {{-- DataTable ends --}}
-
-    {{-- add new sidebar starts --}}
+<div class="row">
     
-    {{-- add new sidebar ends --}}
+  </div>
+  <!-- Zero configuration table -->
+  <section id="basic-datatable">
+      <div class="row">
+          <div class="col-12">
+              <div class="card">
+                  <div class="card-content">
+                      <div class="card-body card-dashboard">
+                          <div class="table-responsive">
+                              <table class="table zero-configuration">
+                                  <thead>
+                                      <tr>
+                                          <th>Name</th>
+                                          <th>Position</th>
+                                          <th>Office</th>
+                                          <th>Age</th>
+                                          <th>Start date</th>
+                                          <th>Salary</th>
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                      
+                                  </tbody>
+                                  <tfoot>
+                                      <tr>
+                                          <th>Name</th>
+                                          <th>Position</th>
+                                          <th>Office</th>
+                                          <th>Age</th>
+                                          <th>Start date</th>
+                                          <th>Salary</th>
+                                      </tr>
+                                  </tfoot>
+                              </table>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   </section>
-  {{-- Data list view end --}}
+  
 @endsection
 @section('vendor-script')
-{{-- vendor js files --}}
-        <script src="{{ asset(mix('vendors/js/extensions/dropzone.min.js')) }}"></script>
+{{-- vendor files --}}
+        <script src="{{ asset(mix('vendors/js/tables/datatable/pdfmake.min.js')) }}"></script>
+        <script src="{{ asset(mix('vendors/js/tables/datatable/vfs_fonts.js')) }}"></script>
         <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.min.js')) }}"></script>
         <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.buttons.min.js')) }}"></script>
-        <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
+        <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.html5.min.js')) }}"></script>
+        <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.print.min.js')) }}"></script>
         <script src="{{ asset(mix('vendors/js/tables/datatable/buttons.bootstrap.min.js')) }}"></script>
-        <script src="{{ asset(mix('vendors/js/tables/datatable/dataTables.select.min.js')) }}"></script>
-        <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.checkboxes.min.js')) }}"></script>
+        <script src="{{ asset(mix('vendors/js/tables/datatable/datatables.bootstrap4.min.js')) }}"></script>
 @endsection
 @section('page-script')
-        {{-- Page js files --}}
-        <script src="{{ asset(mix('js/scripts/ui/data-list-view.js')) }}"></script>
+        {{-- Page js files --}} 
+        <script src="{{ asset(mix('js/scripts/datatables/datatable.js')) }}"></script>
 @endsection
--- resources/views/panels/footer.blade.php
+++ resources/views/panels/footer.blade.php
@@ -4,7 +4,7 @@
 @else
     <footer class="footer {{ $configData['footerType'] }} footer-light">
 @endif
-    <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020<a class="text-bold-800 grey darken-2" href="#" target="_blank">Singonangi,</a>All rights Reserved</span>
+    <p class="clearfix blue-grey lighten-2 mb-0"><span class="float-md-left d-block d-md-inline-block mt-25">COPYRIGHT &copy; 2020<a class="text-bold-800 grey darken-2" href="#" target="_blank">Bagus,</a>All rights Reserved</span>
         <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="feather icon-arrow-up"></i></button>
     </p>
 </footer>
